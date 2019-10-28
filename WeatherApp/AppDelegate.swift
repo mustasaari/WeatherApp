a23d9c27f9e5e05939488bf7f25e94a44507feca
Mikko Mustasaari <c7mmusta@kc408mac10.tamk.ad>
2019-10-28 14:54:38 +0200

    28102019v2

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -12,6 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+        print("restore app state")
+        return true
+    }
+    
+    func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+        print("save app state")
+        return true
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
